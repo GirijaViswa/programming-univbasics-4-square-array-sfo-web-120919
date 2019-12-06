e1d1c77edcfc2458bdf97b0f4de21fa8a4ee1e8d
GIRIJA VISWANATHAN <girijavizhwa@gmail.com>
2019-12-06 23:35:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,17 @@
 def square_array(array)
   # your code here
-  i = 1
-  result = []
-  while i <= array.length do
-  pro = array[i] * array[i]
-  result << pro
-  i += 1
-end
-p result
-return result
+  #i = 1
+  #result = []
+  #while i <= array.length do
+  #pro = array[i] * array[i]
+  #result << pro
+  #i += 1
+#end
+#p result
+#return result
+arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
 
 square_array ([1,2,3])
